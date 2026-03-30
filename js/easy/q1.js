ccd45a7c4ca7581bddbbe1a05e557a85022b1487
awais296azam <awaisazam475@gmail.com>
2026-03-30 15:05:25 +0500

    js updated

---
+++ js/easy/q1.js
@@ -0,0 +1,21 @@
+/*
+Problem: Declare and Print Variable
+
+Description:
+You are given a task to work with variables in JavaScript.
+
+Create a variable named "name" and assign your full name to it.
+Then print the value of the variable using console.log().
+
+Constraints:
+
+* Use let or cost only
+* Do not hardcode output directly inside console.log()
+
+Example:
+Input:
+name = "Awais"
+
+Output:
+Awais
+*/
